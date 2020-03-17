6a066f2582553631ac4e8881c326239a28ba3b42
Prashant Choudhary <Prashant_Choudhary@epam.com>
2020-03-17 16:32:34 +0530

    Refactoring

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-admin',
     templateUrl: './admin.component.html',
-    styleUrls:['./admin.component.css'],
+    styleUrls: ['./admin.component.css'],
 })
 
-export class AdminComponent{
+export class AdminComponent {
 }
